c5420c5e14a5976faf93316fcb05a3b4b1fedd8e
William Alonso <williamalonso@outlook.com>
2023-04-25 14:16:33 -0300

    :sparkles: creating cards component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import NavbarComponent from './components/Navbar/NavbarComponent';
 import CarouselComponent from './components/Carousel/CarouselComponent';
+import CardsComponent from './components/Cards/CardsComponent';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   return (
@@ -7,6 +10,11 @@ function App() {
       <header className="App-header">
         <NavbarComponent/>
         <CarouselComponent/>
+        <Container>
+          <Row className="justify-content-between removeGutter">
+            <CardsComponent/>
+          </Row>
+        </Container>
       </header>
     </div>
   );
---
+++ src/components/Cards/CardsComponent.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import beach from '../../assets/beach.avif';
+import boat from '../../assets/boat.avif';
+import colors from '../../assets/colors.avif';
+
+const CardsComponent = () => {
+
+  const cardsInfo = {
+    cards: [
+      {
+        id: '1',
+        title: 'Título 1',
+        description: 'Descrição 1',
+        img: beach
+      },
+      {
+        id: '2',
+        title: 'Título 2',
+        description: 'Descrição 2',
+        img: boat
+      },
+      {
+        id: '3',
+        title: 'Título 3',
+        description: 'Descrição 3',
+        img: colors
+      }
+    ]
+  }
+
+  return(
+    cardsInfo.cards.map( (e) => {
+      return (
+        <Card key={e.id} style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={e.img} style={{minHeight: '191px'}} />
+          <Card.Body>
+            <Card.Title>{e.title}</Card.Title>
+            <Card.Text>
+              {e.description}
+            </Card.Text>
+            <Button variant="primary">Clique Aqui</Button>
+          </Card.Body>
+        </Card>
+      );
+    })
+  );
+}
+
+export default CardsComponent;
